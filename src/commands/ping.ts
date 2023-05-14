ec69e38b4ff37e3f0a59b6a93083a4be6555feb7
Amu <117264067+salmoenela@users.noreply.github.com>
2023-05-15 04:34:56 +0700

    ping ms fix

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -8,7 +8,7 @@ export default {
   execute(ctx: Context, interaction: APIInteraction): void {
     ctx.response.body = {
       type: InteractionResponseType.ChannelMessageWithSource,
-      data: { content: `Pongu! ${Date.now() - ((Number(interaction.id) >> 22) + 1420070400000)}ms` }
+      data: { content: `Pongu! ${Date.now() - (new Date(Number(BigInt(interaction.id) >> 22n) + 1420070400000))[[Symbol.toPrimitive]('number')]}ms` }
     };
     ctx.response.type = "json";
     ctx.response.status = Status.OK;
