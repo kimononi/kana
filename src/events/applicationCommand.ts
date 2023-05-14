370dc035e13d46800f785b1a6ea1f516a23b93ba
Amu <117264067+salmoenela@users.noreply.github.com>
2023-05-14 18:00:38 +0700

    .

--- src/events/applicationCommand.ts
+++ src/events/applicationCommand.ts
@@ -4,7 +4,7 @@ export default {
   type: InteractionType.ApplicationCommand,
   async execute(ctx: Context, interaction: APIApplicationCommandInteraction): Promise<void> {
     const cmds = Object.values(commands);
-    const command = cmds.find(cmd => cmd.default.data.name === interaction.data.name);
+    const command = cmds.find(cmd => cmd.default.data.name === interaction.data.name).default;
     
     console.log(cmds);
     console.log(command);
