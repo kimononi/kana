143aa7ff8559939613390e7ea5306fdcdd4e5b15
Amu <117264067+salmoenela@users.noreply.github.com>
2023-05-15 04:35:30 +0700

    syntax fix

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -8,7 +8,7 @@ export default {
   execute(ctx: Context, interaction: APIInteraction): void {
     ctx.response.body = {
       type: InteractionResponseType.ChannelMessageWithSource,
-      data: { content: `Pongu! ${Date.now() - (new Date(Number(BigInt(interaction.id) >> 22n) + 1420070400000))[[Symbol.toPrimitive]('number')]}ms` }
+      data: { content: `Pongu! ${Date.now() - (new Date(Number(BigInt(interaction.id) >> 22n) + 1420070400000))[Symbol.toPrimitive]('number')]}ms` }
     };
     ctx.response.type = "json";
     ctx.response.status = Status.OK;
