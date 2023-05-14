64a406354fc92d359aed61d3eaa5de4a7dd73997
Noel <120244424+kruslv@users.noreply.github.com>
2023-05-14 16:56:32 +0700

    commands deploy setup

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,29 @@
-import { Application, InteractionType, Router, sign, Status, STATUS_TEXT } from "./deps.ts";
+import { Application, InteractionType, Router, Routes, RouteBases, sign, Status, STATUS_TEXT } from "./deps.ts";
+import * as commands from "./commands/mod.ts";
 import * as events from "./events/mod.ts";
 
 const router = new Router();
+
+router.get("/", (ctx) => {
+  if (!ctx.request.url.includes(Deno.env.get("DENO_DEPLOYMENT_ID"))) {
+    const statusCode = Status.Unauthorized;
+    ctx.response.body = { code: statusCode, message: STATUS_TEXT[`${statusCode}`] };
+    ctx.response.status = statusCode;
+    ctx.response.type = "json";
+  } else {
+    const response = await fetch(RouteBases + Routes.applicationCommands(Deno.env.get("DISCORD_ID")), {
+      method: "PUT"
+      headers: { Authorization: `Bot ${Deno.env.get("DISCORD_TOKEN")}`, "Content-Type": "application/json" },
+      body: JSON.stringify(Object.values(commands).map(cmd => cmd.default.data))
+    });
+    const result = await response.json();
+
+    ctx.response.body = result;
+    ctx.response.status = response.statusCode;
+    ctx.response.type = "json";
+  }
+});
+
 router.post("/", async (ctx) => {
   const timestamp = ctx.request.headers.get("X-Signature-Timestamp");
   const signature = ctx.request.headers.get("X-Signature-Ed25519");
@@ -15,13 +37,11 @@ router.post("/", async (ctx) => {
 
   if (!valid) {
     const statusCode = Status.Unauthorized;
-    ctx.response.body = STATUS_TEXT[statusCode];
+    ctx.response.body = { code: statusCode, message: STATUS_TEXT[`${statusCode}`] };
     ctx.response.status = statusCode;
+    ctx.response.type = "json";
   } else {
-    const evts = Object.values(events).map(evt => evt.default);
-    const event = evts.find(evt => evt.type === InteractionType.Ping);
-
-    console.log(evts, event);
+    const event = Object.values(events).map(evt => evt.default).find(evt => evt.type === InteractionType.Ping);
     if (event) return await event.execute(ctx, body);
   }
 });
