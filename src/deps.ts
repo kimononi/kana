6cb0bbf7fe378b59bf2d011963716cb66f9b921f
Noel <120244424+kruslv@users.noreply.github.com>
2023-05-14 12:08:12 +0700

    export tweetnacl@1.0.3

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,3 @@
 export * from "https://deno.land/x/discord_api_types@0.37.42/v10.ts";
 export * from "https://deno.land/x/oak@v12.4.0/mod.ts";
+export * from "https://cdn.skypack.dev/tweetnacl@1.0.3";
