4d28d3c181f997ce587c5523e3c3cea5ef6eff35
Amu <117264067+salmoenela@users.noreply.github.com>
2023-05-14 17:45:37 +0700

    debug cmds, command, and interaction

--- src/events/applicationCommand.ts
+++ src/events/applicationCommand.ts
@@ -3,7 +3,13 @@ import * as commands from "../commands/mod.ts";
 export default {
   type: InteractionType.ApplicationCommand,
   async execute(ctx: Context, interaction: APIApplicationCommandInteraction): Promise<void> {
-    const command = Object.values(commands).find(cmd => cmd.default.data.name === interaction.data.name);
+    const cmds = Object.values(commands);
+    const command = cmds.find(cmd => cmd.default.data.name === interaction.data.name);
+    
+    console.log(cmds);
+    console.log(command);
+    console.log(interaction);
+    
     if (command) return await command.execute(ctx, interaction);
   }
 }
