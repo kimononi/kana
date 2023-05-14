c669d5dd03c73e8ae00e9b2a51e528895a3b2afe
Amu <117264067+salmoenela@users.noreply.github.com>
2023-05-14 17:34:21 +0700

    fix async/await errors

--- src/events/applicationCommand.ts
+++ src/events/applicationCommand.ts
@@ -2,8 +2,8 @@ import { APIApplicationCommandInteraction, Context, InteractionType } from "../d
 import * as commands from "../commands/mod.ts";
 export default {
   type: InteractionType.ApplicationCommand,
-  execute(ctx: Context, interaction: APIApplicationCommandInteraction): void {
+  async execute(ctx: Context, interaction: APIApplicationCommandInteraction): Promise<void> {
     const command = Object.values(commands).map(cmd => cmd.default.data.name === interaction.data.name);
-    if (command) await command.execute(ctx, interaction);
+    if (command) return await command.execute(ctx, interaction);
   }
 }
