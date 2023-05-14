bc4bf9756f13d089b053879784ccfe515b2f5000
Amu <117264067+salmoenela@users.noreply.github.com>
2023-05-14 15:03:52 +0700

    add interaction ping interface

--- src/events/ping.ts
+++ src/events/ping.ts
@@ -1,8 +1,8 @@
-import { Context, InteractionResponseType, InteractionType, Status } from "../deps.ts";
+import { APIPingInteraction, Context, InteractionResponseType, InteractionType, Status } from "../deps.ts";
 
 export default {
   type: InteractionType.Ping,
-  execute(ctx: Context): void {
+  execute(ctx: Context, interaction: APIPingInteraction): void {
     ctx.response.body = { type: InteractionResponseType.Pong };
     ctx.response.type = "json";
     ctx.response.status = Status.OK;
