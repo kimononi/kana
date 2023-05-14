668d5d6d476a024250c1aaf8f12b8a8d199b7782
Noel <120244424+kruslv@users.noreply.github.com>
2023-05-14 16:44:45 +0700

    simple command

---
+++ src/commands/ping.ts
@@ -0,0 +1,16 @@
+import { APIInteraction, ApplicationCommandType, Context, InteractionResponseType, Status } from "../deps.ts";
+
+export default {
+  data: {
+    name: "ping",
+    description: "Ping! Pongu!"
+  },
+  execute(ctx: Context, interaction: APIInteraction): void {
+    ctx.response.body = {
+      type: InteractionResponseType.ChannelMessageWithSource,
+      data: { content: "Pongu!" }
+    };
+    ctx.response.type = "json";
+    ctx.status = Status.OK;
+  }
+}
