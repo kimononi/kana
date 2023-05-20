00cb6f12c0bf4a2716b3d50e0ba44e7a24f07607
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-21 05:34:03 +0700

    bring back old route handler

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import * as routes from "./routes/mod.ts";
 
 const router = new Router();
 
-for (const route of Object.values(routes).map(ctx => ctx.default)) {
-  router.add(route.method, route.path, route.middleware);
+for (const { method, path, middleware } of Object.values(routes).map(ctx => ctx.default)) {
+  router.add(method, path, middleware);
 }
 
 const app = new Application();
