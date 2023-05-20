c629ae3e297d2de0ab0116c976334c3b59bc8e77
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-20 19:49:46 +0700

    prepare command data

---
+++ src/commands/config.ts
@@ -0,0 +1,12 @@
+import { Context, PermissionsFlagsBits } from "../deps.ts";
+
+export default {
+  data: {
+    name: "config",
+    description: "Utak atik dulu g sih 🥦",
+    dm_permission: false
+  },
+  async execute(ctx: Context): void {
+
+  }
+}
