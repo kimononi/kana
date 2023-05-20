af30cfb19e9f8a5fbf15ed8a101203faeaef57da
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-20 19:13:44 +0700

    simplify routes handling and remove some function

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,8 @@ import * as routes from "./routes/mod.ts";
 
 const router = new Router();
 
-for (const { method, path, middleware } of Object.values(routes).map(ctx => ctx.default)) {
-  router.add(method, path, middleware);
+for (const route of Object.values(routes).map(ctx => ctx.default)) {
+  router.add(route.method, route.path, route.middleware);
 }
 
 const app = new Application();
@@ -12,9 +12,3 @@ app.use(router.routes());
 app.use(router.allowedMethods());
 
 app.listen({ port: 8080 });
-
-function hexEncode(hex: string): Uint8Array {
-  return new Uint8Array(
-    hex.match(/.{1,2}/g).map(ctx => parseInt(ctx, 16))
-  );
-}
