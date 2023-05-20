e093fb0e5725cda4e6f35ca9888e18b190d6b808
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-21 06:36:40 +0700

    command typeguarding

--- src/events/applicationCommand.ts
+++ src/events/applicationCommand.ts
@@ -1,9 +1,12 @@
 import { APIApplicationCommandInteraction, Context, InteractionType } from "../deps.ts";
 import * as commands from "../commands/mod.ts";
+
 export default {
   type: InteractionType.ApplicationCommand,
   async execute(ctx: Context, interaction: APIApplicationCommandInteraction): Promise<void> {
-    const command = Object.values(commands).find(cmd => cmd.default.data.name === interaction.data.name).default;
-    if (command) return await command.execute(ctx, interaction);
+    if (interaction.type === InteractionType.ChatInput) {
+      const command = Object.values(commands).find(cmd => cmd.default.data.name === interaction.data.name && cmd.default.data.type === interaction.data.type).default;
+      if (command) return await command.execute(ctx, interaction);
+    }
   }
 }
