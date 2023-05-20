5e2a1bf5c3c7263b0fb9200baf15d08a003355fe
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-20 20:05:36 +0700

    permissions checking script test

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -1,4 +1,4 @@
-import { Context, PermissionsFlagsBits } from "../deps.ts";
+import { APIChatInputApplicationCommandInteractionData, Context, InteractionResponseType, PermissionsFlagsBits, Status } from "../deps.ts";
 
 export default {
   data: {
@@ -6,7 +6,24 @@ export default {
     description: "Utak atik dulu g sih 🥦",
     dm_permission: false
   },
-  async execute(ctx: Context): void {
-
+  async execute(ctx: Context, interaction: APIChatInputApplicationCommandInteractionData): Promise<void> {
+    ctx.response.status = StatusOK;
+    ctx.response.type = "json";
+    
+    if ((interaction.member.permissions & PermissionsFlagsBits.ManageGuild) !== PermissionsFlagsBits.ManageGuild) {
+      ctx.response.body = {
+        type: InteractionResponseType.ChannelMessageWithSource,
+        data: {
+          content: "minimal punya izin `Kelola Server` deck."
+        }
+      };
+    } else {
+      ctx.response.body = {
+        type: InteractionResponseType.ChannelMessageWithSource,
+        data: {
+          content: "new feature coming soon (\\*´ω｀\\*)"
+        }
+      }
+    }
   }
 }
