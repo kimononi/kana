a8e2e92c723ab344882d41696082662b9d99e63f
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-21 15:50:39 +0700

    fix modal finder method

--- src/events/modalSubmit.ts
+++ src/events/modalSubmit.ts
@@ -4,7 +4,7 @@ import * as modals from "../modals/mod.ts";
 export default {
   type: InteractionType.ModalSubmit,
   async execute(ctx: Context, interaction: APIModalSubmitInteraction): Promise<any> {
-    const modal = Object.values(modals).map(modal => modal.default.custom_id === interaction.data.custom_id).default;
+    const modal = Object.values(modals).find(modal => modal.default.custom_id === interaction.data.custom_id).default;
     console.log(modal);
     
     if (modal) return await modal.execute(ctx, interaction);
