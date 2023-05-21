054b633bcc77945cf1f9354219fe56bdedeb365e
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-21 16:15:38 +0700

    import interaction response type

--- src/modals/confess.ts
+++ src/modals/confess.ts
@@ -1,4 +1,4 @@
-import { APIModalSubmitInteraction, CDNRoutes, Context, MessageFlags, MongoClient, RouteBases, Routes, Snowflake, Status } from "../deps.ts";
+import { APIModalSubmitInteraction, CDNRoutes, Context, InteractionResponseType, MessageFlags, MongoClient, RouteBases, Routes, Snowflake, Status } from "../deps.ts";
 
 export default {
   custom_id: "confess",
