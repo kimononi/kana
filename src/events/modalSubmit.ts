9b466eb0089a2a99369a61183696f1695bddd881
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-21 13:39:46 +0700

    update interaction instance and modal handler

--- src/events/modalSubmit.ts
+++ src/events/modalSubmit.ts
@@ -1,10 +1,10 @@
-import { APIModalSubmission, Context, InteractionType } from "../deps.ts";
+import { APIModalSubmitInteraction, Context, InteractionType } from "../deps.ts";
 import * as modals from "../modals/mod.ts";
 
 export default {
   type: InteractionType.ModalSubmit,
-  async execute(ctx: Context, interaction: APIModalSubmission): Promise<any> {
-    const modal = Object.values(modals).map(mdl => mdl.default.data.custom_id === interaction.custom_id).default;
+  async execute(ctx: Context, interaction: APIModalSubmitInteraction): Promise<any> {
+    const modal = Object.values(modals).map(modal => modal.default.custom_id === interaction.data.custom_id).default;
     if (modal) return await modal.execute(ctx, interaction);
   }
 }
