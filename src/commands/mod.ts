c6d79aa6edf91d646dab5e4c4ee7945fc37dfb5d
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-21 10:10:07 +0700

    dev (#4)

--- src/commands/mod.ts
+++ src/commands/mod.ts
@@ -1,2 +1,3 @@
+export * as confess from "./confess.ts";
 export * as config from "./config.ts";
 export * as ping from "./ping.ts";
--- src/events/applicationCommand.ts
+++ src/events/applicationCommand.ts
@@ -1,9 +1,10 @@
 import { APIApplicationCommandInteraction, Context, InteractionType } from "../deps.ts";
 import * as commands from "../commands/mod.ts";
+
 export default {
   type: InteractionType.ApplicationCommand,
   async execute(ctx: Context, interaction: APIApplicationCommandInteraction): Promise<void> {
-    const command = Object.values(commands).find(cmd => cmd.default.data.name === interaction.data.name).default;
+    const command = Object.values(commands).find(cmd => cmd.default.data.name === interaction.data.name && cmd.default.data.type === interaction.data.type).default;
     if (command) return await command.execute(ctx, interaction);
   }
 }
