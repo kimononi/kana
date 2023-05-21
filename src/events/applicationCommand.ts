97496671744e0b3e91f05b14c8980d485e290de3
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-21 09:54:27 +0700

    fix interaction comparison

--- src/events/applicationCommand.ts
+++ src/events/applicationCommand.ts
@@ -4,9 +4,7 @@ import * as commands from "../commands/mod.ts";
 export default {
   type: InteractionType.ApplicationCommand,
   async execute(ctx: Context, interaction: APIApplicationCommandInteraction): Promise<void> {
-    if (interaction.type === InteractionType.ChatInput) {
-      const command = Object.values(commands).find(cmd => cmd.default.data.name === interaction.data.name && cmd.default.data.type === interaction.data.type).default;
-      if (command) return await command.execute(ctx, interaction);
-    }
+    const command = Object.values(commands).find(cmd => cmd.default.data.name === interaction.data.name && cmd.default.data.type === interaction.data.type).default;
+    if (command) return await command.execute(ctx, interaction);
   }
 }
