8b286d402d52975503d956907202db05fc9f25b7
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-21 17:16:20 +0700

    remove modal debug

--- src/events/modalSubmit.ts
+++ src/events/modalSubmit.ts
@@ -5,8 +5,6 @@ export default {
   type: InteractionType.ModalSubmit,
   async execute(ctx: Context, interaction: APIModalSubmitInteraction): Promise<any> {
     const modal = Object.values(modals).find(modal => modal.default.custom_id === interaction.data.custom_id).default;
-    console.log(modal);
-    
     if (modal) return await modal.execute(ctx, interaction);
   }
 }
