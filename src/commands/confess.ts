3ce423ddeeeb65b4be69d9ccb7dbc40045e621ed
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-21 08:56:10 +0700

    confess channel check test

--- src/commands/confess.ts
+++ src/commands/confess.ts
@@ -1,4 +1,4 @@
-import { APIUserApplicationCommandInteractionData, ApplicationCommandType, Context } from "../deps.ts";
+import { APIUserApplicationCommandInteractionData, ApplicationCommandType, Context, InteractionResponseType, MessageFlags, MongoClient, RouteBases, Routes } from "../deps.ts";
 
 export default {
   data: {
@@ -7,5 +7,28 @@ export default {
     type: ApplicationCommandType.User,
     dm_permission: false
   },
-  async execute(ctx: Context, interaction: APIUserApplicationCommandInteractionData): void {}
+  async execute(ctx: Context, interaction: APIUserApplicationCommandInteractionData): Promise<void> {
+    const mongo = new MongoClient();
+    await mongo.connect(Deno.env.get("MONGO_URI"));
+    
+    const confessChannel = mongo.database("guild").collection<Config>("config").findOne({ _id: interaction.guild_id });
+    if (!confessChannel) {
+      ctx.response.body = {
+        type: InteractionResponseType.ChannelMessageWithSource,
+        data: {
+          flags: MessageFlags.Ephemeral,
+          content: `🍥゛Admin disini belum ngasih tau gwe channel confess dimane`
+        }
+      };
+    } else {
+      // const confessResult = await fetch(RouteBases.api + Routes.channelMessage());
+      ctx.response.body = {
+        type: InteractionResponseType.ChannelMessageWithSource,
+        data: {
+          flags: MessageFlags.Ephemeral,
+          content: `🍥゛Ciailah, ngebet amat pengen confess, sabar dikit nape, lagi di bikin sistem nye`
+        }
+      };
+    }
+  }
 }
