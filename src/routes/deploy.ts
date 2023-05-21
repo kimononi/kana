8b411df2e8e474832de39ebd87a0dadd46883f44
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-21 15:01:14 +0700

    update deploy route path

--- src/routes/deploy.ts
+++ src/routes/deploy.ts
@@ -2,7 +2,7 @@ import { Context, RouteBases, Routes, Status, STATUS_TEXT } from "../deps.ts";
 import * as commands from "../commands/mod.ts";
 
 export default {
-  path: "/",
+  path: "/deploy",
   method: "GET",
   async middleware(ctx: Context): Promise<void> {
     ctx.response.type = "json";
