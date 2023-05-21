0228158b8188440431357d335c559df1f4c85472
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-21 13:12:14 +0700

    modals handler setup

--- src/events/modalSubmit.ts
+++ src/events/modalSubmit.ts
@@ -1,6 +1,10 @@
-import { Context, InteractionType } from "../deps.ts";
+import { APIModalSubmission, Context, InteractionType } from "../deps.ts";
+import * as modals from "../modals/mod.ts";
 
 export default {
   type: InteractionType.ModalSubmit,
-  async execute(ctx: Context): void {}
+  async execute(ctx: Context, interaction: APIModalSubmission): Promise<any> {
+    const modal = Object.values(modals).map(mdl => mdl.default.data.custom_id === interaction.custom_id).default;
+    if (modal) return await modal.execute(ctx, interaction);
+  }
 }
