2441632812929516d738d34117d6bc4e74fb1e7a
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-21 15:48:15 +0700

    debug modal object

--- src/events/modalSubmit.ts
+++ src/events/modalSubmit.ts
@@ -5,6 +5,8 @@ export default {
   type: InteractionType.ModalSubmit,
   async execute(ctx: Context, interaction: APIModalSubmitInteraction): Promise<any> {
     const modal = Object.values(modals).map(modal => modal.default.custom_id === interaction.data.custom_id).default;
+    console.log(modal);
+    
     if (modal) return await modal.execute(ctx, interaction);
   }
 }
