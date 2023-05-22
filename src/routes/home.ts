a944d4e0dc3ca1ac155af908c886ddc970ef6058
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-22 13:45:51 +0700

    define redirect URI

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -4,7 +4,7 @@ export default {
   path: "/",
   method: "GET",
   async middleware(ctx: Context): Promise<void> {
-    const redirectURI = "";
+    const redirectURI = ctx.request.url.origin + "/auth";
     
     if (!ctx.cookies.has("access_token") || !ctx.cookies.has("refresh_token")) {
       ctx.response.redirect(redirectURI);
