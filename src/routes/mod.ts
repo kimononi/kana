989aa97c0d148e9f36311a464e74bfbf4028d3ea
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-22 15:03:11 +0700

    export home route

--- src/routes/mod.ts
+++ src/routes/mod.ts
@@ -1,4 +1,5 @@
 export * as auth from "./auth.ts";
 export * as deploy from "./deploy.ts";
 export * as fetchUser from "./fetchUser.ts";
+export * as home from "./home.ts";
 export * as interaction from "./interaction.ts";
