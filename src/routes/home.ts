11fc4424ff53568e96d231e17f77d24667ae4048
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-22 14:04:07 +0700

    update redirect uri

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,15 +1,21 @@
-import { Context } from "../deps.ts";
+import { Context, OAuth2Routes, OAuth2Scopes } from "../deps.ts";
 
 export default {
   path: "/",
   method: "GET",
   async middleware(ctx: Context): Promise<void> {
-    const redirectURI = ctx.request.url.origin + "/auth";
+    const scopes = [OAuth2Scopes.Identify];
+    
+    const redirectURI = new URL(OAuth2Routes.authorizationURL);
+    redirectURI.searchParams.set("client_id", Deno.env.get("DISCORD_ID"));
+    redirectURI.searchParams.set("redirect_uri", ctx.request.url.origin + "/auth");
+    redirectURI.searchParams.set("response_type", "code");
+    redirectURI.searchParams.set("scope", scopes.join(" "));
     
     if (!ctx.cookies.has("access_token") || !ctx.cookies.has("refresh_token")) {
       ctx.response.redirect(redirectURI);
     } else {
-    
+      
     }
   }
 }
