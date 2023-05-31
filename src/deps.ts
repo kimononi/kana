fc4b186b833f76846138b960bc66c4c66ff3fde9
iciepex <120244424+ich1fx@users.noreply.github.com>
2023-05-31 21:07:54 +0700

    upgrade discord api version

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
-export * from "https://deno.land/x/discord_api_types@0.37.42/v9.ts";
+export * from "https://deno.land/x/discord_api_types@0.37.42/v10.ts";
 export * from "https://deno.land/x/oak@v12.4.0/mod.ts";
 export * from "https://cdn.skypack.dev/tweetnacl@1.0.3";
 export * from "https://deno.land/x/mongo@v0.31.2/mod.ts";
